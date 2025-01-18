1de99e4486ee676d5f3160b360f8c1bcdabc53d5
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-01-18 16:35:24 +0500

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useRef, useState } from "react"
+import "./App.scss"
+import seven from "./slots/SEVEN.png"
+import lemon from "./slots/LEMON.png"
+import bar from "./slots/BAR.png"
+import grape from "./slots/GRAPE.png"
+import bell from "./slots/BELL.png"
+import cherry from "./slots/CHERRY.png"
+import logo from "./logo.png"
+
+export const App = () => {
+    const [spin, setSpin] = useState(false)
+    const [win, setWin] = useState(0)
+    const [bet, setBet] = useState(10)
+    const [betState, setBetState] = useState(true)
+    const [combination, setCombination] = useState<string[]>(["", "", ""])
+    const [balance, setBalance] = useState(100)
+
+    const slotRefs = [useRef(null), useRef(null), useRef(null)]
+
+    const images = [seven, lemon, bar, grape, bell, cherry]
+    const symbols = ["seven", "lemon", "bar", "grape", "bell", "cherry"]
+    const winFactor: [string, number][] = [["seven", 259], ["lemon", 5], ["bar", 10], ["bell", 9], ["grape", 7], ["cherry", 3]]
+
+    function winCounting(bet: string[]): number {
+        let total = 0
+        winFactor.map(el => {
+            const count = bet.filter(value => value == el[0]).length
+            if (count == 3) {
+                total += 3 * el[1]
+            } else if (count == 2 && el[0] == "seven") {
+                total += 12
+            } else if (count == 2) {
+                total += el[1]
+            } return 0
+        })
+        setCombination(["", "", ""])
+        return total
+    }
+
+    function startSpin(ref: React.RefObject<any>, delay: number, index: number) {
+        let intervalId: NodeJS.Timeout
+
+        const spinSlot = () => {
+            const random = Math.floor(Math.random() * images.length)
+            ref.current.src = images[random]
+            setCombination((prev) => {
+                const newCombination = [...prev];
+                newCombination[index] = symbols[random]
+                return newCombination
+            })
+        }
+
+        intervalId = setInterval(spinSlot, 30)
+
+        setTimeout(() => {
+            clearInterval(intervalId);
+            ref.current.style.animationName = ""
+        }, delay)
+    }
+
+    useEffect(() => {
+        if (spin) {
+            const delays = [900, 1800, 2700]
+
+            slotRefs.forEach((ref, index) => {
+                if (ref.current) {
+                    ref.current.style.animationName = "spin"
+                    startSpin(ref, delays[index], index)
+                }
+            });
+            setTimeout(() => {
+                setSpin(false)
+            }, Math.max(...delays) + 500)
+        }
+        setBalance((prev) => prev + winCounting(combination) * bet)
+        setWin(winCounting(combination) * bet)
+    }, [spin])
+
+    const handlerBet = (value: number) => () => {
+           if ((bet + value > balance)) {
+            setBet(balance)
+           } else if ((bet + value) < 1) {
+            setBet(1)
+           } else if ((bet + value) > 999) {
+            setBet(999)
+           } else {
+            setBet(bet + value)
+           }
+        }
+
+    return (
+        <>
+            <header>
+                <img src={logo} />
+            </header>
+            <div id="automat">
+                <div id="slots">
+                    {slotRefs.map((ref, index) => (
+                        <img src={seven} ref={ref} key={index} />
+                    ))}
+                </div>
+                <div id="interface">
+                    <div className="info">Win: {win == 0 ? "" : `${win}$`}</div>
+                    <div className="info">Balance: {balance}$</div>
+                    <button className="go-button" onClick={() => {balance > 0 && setSpin(true); balance > 0 && setBalance(balance - bet)}} disabled={spin}></button>
+                    <div className="bet">
+                        <span>Bet: {bet}$
+                            <button className="change" onClick={() => setBetState(true)}>+</button>
+                            <button className="change" onClick={() => setBetState(false)}>-</button>
+                        </span>
+                        {betState ? <button onClick={handlerBet(1)} className="bet-state" disabled={spin}>+1$</button> : <button onClick={handlerBet(-1)} className="bet-state" disabled={spin}>-1$</button>}
+                        {betState ? <button onClick={handlerBet(10)} className="bet-state" disabled={spin}>+10$</button> : <button onClick={handlerBet(-10)} className="bet-state" disabled={spin}>-10$</button>}
+                        {betState ? <button onClick={handlerBet(100)} className="bet-state" disabled={spin}>+100$</button> : <button onClick={handlerBet(-100)} className="bet-state" disabled={spin}>-100$</button>}
+                    </div>              
+                    <button className="info">History</button>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ webpack.config.ts
@@ -0,0 +1,62 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack, {Configuration} from 'webpack';
+import {Configuration as DevServerConfiguration} from 'webpack-dev-server';
+
+type Mode = "production" | "development";
+
+interface EnvInterface {
+    mode: Mode;
+    port: number;
+}
+
+const config = (env: EnvInterface): webpack.Configuration => {
+    const config: webpack.Configuration = {
+        mode: env.mode,
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].js',
+            assetModuleFilename: 'assets/[name][ext]',
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html'),
+            }),
+            new webpack.ProgressPlugin()
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpe?g|gif|jpg)$/i,
+                    type: 'asset/resource',
+                  },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                      "style-loader",
+                      "css-loader",
+                      "sass-loader",
+                    ],
+                  },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        devServer: {
+            port: env.port,
+            open: true,
+        },
+    };
+
+    return config;
+};
+
+export default config;
